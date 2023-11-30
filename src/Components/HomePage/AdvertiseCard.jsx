eda48a4032ff18728042e9091838299591f05793
Al Mamun <wdmamun75@gmail.com>
2023-11-30 08:07:20 +0600

    addded one commit

--- src/Components/HomePage/AdvertiseCard.jsx
+++ src/Components/HomePage/AdvertiseCard.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 
 const AdvertieseCard = ({ item }) => {
 
-    const { startPrice, advertiseId, endPrice, status, propertyImage, propertyTitle, propertyLocation, agentName, agentImage } = item || {}
+    const { startPrice, advertiseId, endPrice, status, propertyImage, propertyTitle, propertyLocation, agentName } = item || {}
 
     return (
         <div className="flex flex-col justify-center items-center">
